52d0d5c28bea1166bed883873d14de7704eee2b2
Nanda241998 <117671873+Nanda241998@users.noreply.github.com>
2023-02-11 21:43:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-/** @format */
+const studentArray = [
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  function PrintStudentswithMap() {
+    //Write your code here , just console.log
+    const filteredStudents = studentArray.filter(student => student.marks >= 50);
+    filteredStudents.map(student => console.log(student));
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+    studentArray.forEach(student => {
+      if(student.marks >= 50){
+        console.log(student);
+      }
+    });
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    
+    let name = nameInput.value;
+    let marks = marksInput.value;
+    let age = ageInput.value;
 
-let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
+    if (name != "" && marks != "" && age != "") {
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
+        // create a new obj from the received value and
+        const newObj = {
+            id:4,name:"susan",age:"20",marks:"45"
+        }
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
+        //  then add it to the existing userData array
+        userData.push(newObj);
+        console.log(userData)
+        listOnPage.innerHTML = "";
+        userData.forEach((item) => {
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = `${item.id} &nbsp;&nbsp; Name: ${item.name} &nbsp; Age: ${item.age}  &nbsp; marks: ${item.marks}`;
+            listOnPage.append(newDiv);
+        })
+    }
+  }
+  
 
-function addData() {
-  //Write your code here, just console.log
-}
 
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
+       
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+    const updatedArray = studentArray.filter(student => student.marks >= 50);
+    console.log(updatedArray);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    const newArray = [{id:5, name:q"dave", age:"22", marks:60},{id:6, name:"elia", age:"18", marks:70},{id:7, name:"bob", age:"20", marks:80}];
 
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+   const concatenateArray = studentArray.concat(newArray);
+   console.log(concatenateArray);
+  }
+  
